e9aa9880093ed77249f722a467a80187de27452c
Juan Manuel Gomez Montero <jmgomez@DGE.GOB>
2026-05-18 11:08:05 -0300

    Editar usuario

--- habits/urls.py
+++ habits/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import persona
+from . import views
 
 urlpatterns = [
     path('',persona),
+    path('personas/',persona, name='lista_personas'),
+    path('modificar/<int:id>/', views.modificarPersona, name='modificarPersona'),
 ]
