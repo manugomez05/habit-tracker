5f37b27fc68932841e3f295139579c989e35a9db
Valentin Emanuel Motos <vmotos@DGE.GOB>
2026-05-18 10:21:10 -0300

    formulario

--- habits/views.py
+++ habits/views.py
@@ -1,8 +1,10 @@
 from django.template import loader
 from django.http import HttpResponse
 from .models import Persona
-# Create your views here.
+from django.shortcuts import redirect,render
+from .forms import PersonaForm
 
+# Create your views here.
 def persona(request):
     personas= Persona.objects.all().values()
     template = loader.get_template('persona.html')
@@ -10,3 +12,16 @@ def persona(request):
         'personas': personas,
     }
     return HttpResponse(template.render(context,request))
+
+def crearPersona(request):
+    if request.method == 'POST':
+
+        form = PersonaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_personas')
+    else:
+
+        form = PersonaForm()
+        context = {'form':form}
+    return render(request, 'habits/crear_persona.html', context)
