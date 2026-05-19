700188fe09afc51795a9071b8c0c2acd670fb70e
Valentin Emanuel Motos <vmotos@DGE.GOB>
2026-05-19 10:26:27 -0300

    cambio en los models

--- habits/models.py
+++ habits/models.py
@@ -2,18 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Habitos(models.Model):
-    titulo = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True)
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-
-    owner = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE
-    )
-
-    def __str__(self):
-        return self.title
 class Persona(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
@@ -28,3 +16,12 @@ class Grupo(models.Model):
 
     def __str__(self):
         return self.nombre
+class Habitos(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.titulo
